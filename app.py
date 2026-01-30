51bf656a33a0f2f567aa060a83d18a7b5405081a
oliviawyy <olivia.c@aluno.senai.br>
2026-01-30 17:20:55 -0300

    quase acabando

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,26 @@ def pagina_principal():
 @app.route("/sobre")
 def pagina_sobre():
     return render_template("sobre.html")
+
+@app.route("/login", methods=["GET"])
+def pagina_login():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form.get("email")
+    senha = request.form.get("senha")
+
+    if email == "oie@gmail.com" and senha == "123":
+        return "Voce acessou a pagina restrita"
+    else:
+        return render_template("login.html", erro = "Acesso Negado!")
+
+# PAGINA RESTRITA
+@app.route("/comentarios")
+def pagina_comentario():
+    return render_template("comentarios.html")
+
 
 app.run(debug=True)
 
