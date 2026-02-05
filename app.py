8b64a5c5747d58c4337025b5701bf8e3726714cf
oliviawyy <olivia.c@aluno.senai.br>
2026-02-05 17:06:43 -0300

    site

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
+app.secret_key = "banana"
+lista_de_comentarios = []
+
+
 @app.route("/")
 def pagina_principal():
     return render_template("principal.html")
@@ -19,15 +23,22 @@ def login_post():
     email = request.form.get("email")
     senha = request.form.get("senha")
 
-    if email == "oie@gmail.com" and senha == "123":
-        return "Voce acessou a pagina restrita"
+    if email == "oii@gmail.com" and senha == "1234":
+        return redirect("/comentarios")
     else:
         return render_template("login.html", erro = "Acesso Negado!")
 
 # PAGINA RESTRITA
 @app.route("/comentarios")
-def pagina_comentario():
-    return render_template("comentarios.html")
+def pagina_comentarios():
+    return render_template("comentarios.html", lista_de_comentarios = lista_de_comentarios)
+
+@app.route("/adicionar_comentario", methods = ["POST"])
+def adicionar_comentario():
+    comentario =  request.form.get("comentario")
+    lista_de_comentarios.append(comentario)
+    print(lista_de_comentarios)
+    return redirect("/comentarios")
 
 
 app.run(debug=True)
